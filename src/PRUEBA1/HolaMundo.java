e1f05a8ca499adf2107192eaad7ac553f9b921d7
ReeDaa20 <redajose@gmail.com>
2017-05-05 12:05:06 +0200

    Update HolaMundo.java

--- src/PRUEBA1/HolaMundo.java
+++ src/PRUEBA1/HolaMundo.java
@@ -5,5 +5,6 @@ public class HolaMundo {
 	public static void main(String[] args) {
 		System.out.println("No va esta mierda");
 		System.out.println("jajaja xd");
+		System.out.println("Jejejeje xD");
 	}
 }
